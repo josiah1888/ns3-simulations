912c228bd89d67d8df17a5716ab15a417da9eb2c
Josiah Burchard <muitprogram@mwc-076116.dhcp.missouri.edu>
2016-06-13 11:11:34 -0500

    Adding mobility model, fixing animation

--- src/mesh/examples/mesh.cc
+++ src/mesh/examples/mesh.cc
@@ -63,8 +63,9 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("TestMeshScript");
 
-/*  Terminal commands
-*   clear && ./waf --run "mesh --x-size=30 --y-size=3 --time=250 --packet-interval=5 --packet-size=1 --step=100"
+/**  Terminal commands
+ *   clear && ./waf --run "mesh --x-size=3 --y-size=5 --time=250 --packet-interval=60 --packet-size=1024 --step=100 --pause=0"
+ *   git add src/mesh/examples/mesh.cc
 */
 
 
@@ -85,8 +86,11 @@ private:
   double    m_randomStart;
   double    m_totalTime;
   double    m_packetInterval;
+  double    m_speed;
+  double    m_pause;
   uint16_t  m_packetSize;
   uint32_t  m_nIfaces;
+  uint32_t  m_nNodes;
   bool      m_chan;
   bool      m_pcap;
   std::string m_stack;
@@ -116,8 +120,11 @@ MeshTest::MeshTest () :
   m_randomStart (0.1),
   m_totalTime (100.0),
   m_packetInterval (0.1),
+  m_speed (2.0),
+  m_pause (1.0),
   m_packetSize (1024),
   m_nIfaces (1),
+  m_nNodes (15),
   m_chan (true),
   m_pcap (false),
   m_stack ("ns3::Dot11sStack"),
@@ -135,11 +142,14 @@ MeshTest::Configure (int argc, char *argv[])
    * As soon as starting node means that it sends a beacon,
    * simultaneous start is not good.
    */
+  cmd.AddValue ("speed", "Speed of each node", m_speed);
+  cmd.AddValue ("pause", "Time for each node to pause", m_pause);
   cmd.AddValue ("start",  "Maximum random start delay, seconds. [0.1 s]", m_randomStart);
   cmd.AddValue ("time",  "Simulation time, seconds [100 s]", m_totalTime);
   cmd.AddValue ("packet-interval",  "Interval between packets in UDP ping, seconds [0.001 s]", m_packetInterval);
   cmd.AddValue ("packet-size",  "Size of packets in UDP ping", m_packetSize);
   cmd.AddValue ("interfaces", "Number of radio interfaces used by each mesh point. [1]", m_nIfaces);
+  cmd.AddValue ("nodes", "Number of nodes in mesh", m_nNodes);
   cmd.AddValue ("channels",   "Use different frequency channels for different interfaces. [0]", m_chan);
   cmd.AddValue ("pcap",   "Enable PCAP traces on interfaces. [0]", m_pcap);
   cmd.AddValue ("stack",  "Type of protocol stack. ns3::Dot11sStack by default", m_stack);
@@ -151,11 +161,8 @@ MeshTest::Configure (int argc, char *argv[])
 }
 void
 MeshTest::CreateNodes ()
-{ 
-  /*
-   * Create m_ySize*m_xSize stations to form a grid topology
-   */
-  nodes.Create (m_ySize*m_xSize);
+{
+  nodes.Create (m_nNodes);
   // Configure YansWifiChannel
   YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
   YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
@@ -189,17 +196,38 @@ MeshTest::CreateNodes ()
   mesh.SetNumberOfInterfaces (m_nIfaces);
   // Install protocols and return container if MeshPointDevices
   meshDevices = mesh.Install (wifiPhy, nodes);
-  // Setup mobility - static grid topology
-  MobilityHelper mobility;
-  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
-                                 "MinX", DoubleValue (0.0),
-                                 "MinY", DoubleValue (0.0),
-                                 "DeltaX", DoubleValue (m_step),
-                                 "DeltaY", DoubleValue (m_step),
-                                 "GridWidth", UintegerValue (m_xSize),
-                                 "LayoutType", StringValue ("RowFirst"));
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (nodes);
+
+  // Mobility
+  MobilityHelper mobilityAdhoc;
+  int64_t streamIndex = 0; // used to get consistent mobility across scenarios
+
+  ObjectFactory pos;
+  pos.SetTypeId ("ns3::RandomRectanglePositionAllocator");
+  std::stringstream ssX;
+  ssX << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_xSize * m_step << "]";
+  std::cout << "x here " << ssX.str();
+  pos.Set ("X", StringValue (ssX.str ()));
+
+  std::stringstream ssY;
+  ssY << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_ySize * m_step << "]";
+  std::cout << ssY.str();
+  pos.Set ("Y", StringValue (ssY.str ()));
+
+  Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
+  streamIndex += taPositionAlloc->AssignStreams (streamIndex);
+
+  std::stringstream ssSpeed;
+  ssSpeed << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_speed << "]";
+  std::stringstream ssPause;
+  ssPause << "ns3::ConstantRandomVariable[Constant=" << m_pause << "]";
+  mobilityAdhoc.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
+                                  "Speed", StringValue (ssSpeed.str ()),
+                                  "Pause", StringValue (ssPause.str ()),
+                                  "PositionAllocator", PointerValue (taPositionAlloc));
+  mobilityAdhoc.SetPositionAllocator (taPositionAlloc);
+  mobilityAdhoc.Install (nodes);
+  streamIndex += mobilityAdhoc.AssignStreams (nodes, streamIndex);
+
   if (m_pcap)
     wifiPhy.EnablePcapAll (std::string ("mp-"));
 }
@@ -226,7 +254,7 @@ MeshTest::InstallApplication ()
   // clientApp.Start (Seconds (0.0));
   // clientApp.Stop (Seconds (m_totalTime));
 
-  for(int i = 0; i < m_xSize * m_ySize; i++) {
+  for(int i = 0; i < m_nNodes; i++) {
     if (i != destIdx) {
       UdpClientHelper client (interfaces.GetAddress (0), 9);
       client.SetAttribute ("MaxPackets", UintegerValue ((uint32_t)(m_totalTime*(1/m_packetInterval))));
@@ -244,10 +272,12 @@ MeshTest::Run ()
   CreateNodes ();
   InstallInternetStack ();
   InstallApplication ();
-  //Simulator::Schedule (Seconds (m_totalTime), &MeshTest::Report, this);
+  Simulator::Schedule (Seconds (m_totalTime), &MeshTest::Report, this);
   Simulator::Stop (Seconds (m_totalTime));
 
   PrintMap();
+
+  AnimationInterface anim ("mesh-animation.xml");
 
   Simulator::Run ();
   Simulator::Destroy ();
@@ -271,6 +301,7 @@ MeshTest::Report ()
         }
       mesh.Report (*i, of);
       of.close ();
+      break;
     }
 }
 
