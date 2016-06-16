dac45acdeac84ad09c2f6665833438d02fe0e4c3
Josiah Burchard <muitprogram@mwc-076148.dhcp.missouri.edu>
2016-06-16 15:44:33 -0500

    Adding experiments in scratch

---
+++ scratch/hwmp-mesh.cc
@@ -0,0 +1,324 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008,2009 IITP RAS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Kirill Andreev <andreev@iitp.ru>
+ *
+ *
+ * By default this script creates m_xSize * m_ySize square grid topology with
+ * IEEE802.11s stack installed at each node with peering management
+ * and HWMP protocol.
+ * The side of the square cell is defined by m_step parameter.
+ * When topology is created, UDP ping is installed to opposite corners
+ * by diagonals. packet size of the UDP ping and interval between two
+ * successive packets is configurable.
+ * 
+ *  m_xSize * step
+ *  |<--------->|
+ *   step
+ *  |<--->|
+ *  * --- * --- * <---Ping sink  _
+ *  | \   |   / |                ^
+ *  |   \ | /   |                |
+ *  * --- * --- * m_ySize * step |
+ *  |   / | \   |                |
+ *  | /   |   \ |                |
+ *  * --- * --- *                _
+ *  ^ Ping source
+ *
+ *  See also MeshTest::Configure to read more about configurable
+ *  parameters.
+ */
+
+
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "ns3/mesh-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/mesh-helper.h"
+
+#include "ns3/netanim-module.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE ("TestMeshScript");
+
+/**  Terminal commands
+ *   clear && ./waf --run "mesh --x-size=3 --y-size=5 --time=250 --packet-interval=60 --packet-size=1024 --step=100 --pause=0"
+ *   git add scratch/hwmp-mesh.cc scratch/mobicom_expr.cc
+*/
+
+
+class MeshTest
+{
+public:
+  /// Init test
+  MeshTest ();
+  /// Configure test from command line arguments
+  void Configure (int argc, char ** argv);
+  /// Run test
+  int Run ();
+  void PrintMap();
+private:
+  int       m_xSize;
+  int       m_ySize;
+  double    m_step;
+  double    m_randomStart;
+  double    m_totalTime;
+  double    m_packetInterval;
+  double    m_speed;
+  double    m_pause;
+  uint16_t  m_packetSize;
+  uint32_t  m_nIfaces;
+  uint32_t  m_nNodes;
+  bool      m_chan;
+  bool      m_pcap;
+  std::string m_stack;
+  std::string m_root;
+  /// List of network nodes
+  NodeContainer nodes;
+  /// List of all mesh point devices
+  NetDeviceContainer meshDevices;
+  //Addresses of interfaces:
+  Ipv4InterfaceContainer interfaces;
+  // MeshHelper. Report is not static methods
+  MeshHelper mesh;
+private:
+  /// Create nodes and setup their mobility
+  void CreateNodes ();
+  /// Install internet m_stack on nodes
+  void InstallInternetStack ();
+  /// Install applications
+  void InstallApplication ();
+  /// Print mesh devices diagnostics
+  void Report ();
+};
+MeshTest::MeshTest () :
+  m_xSize (3),
+  m_ySize (3),
+  m_step (100.0),
+  m_randomStart (0.1),
+  m_totalTime (100.0),
+  m_packetInterval (0.1),
+  m_speed (2.0),
+  m_pause (1.0),
+  m_packetSize (1024),
+  m_nIfaces (1),
+  m_nNodes (15),
+  m_chan (true),
+  m_pcap (false),
+  m_stack ("ns3::Dot11sStack"),
+  m_root ("ff:ff:ff:ff:ff:ff")
+{
+}
+void
+MeshTest::Configure (int argc, char *argv[])
+{
+  CommandLine cmd;
+  cmd.AddValue ("x-size", "Number of nodes in a row grid. [6]", m_xSize);
+  cmd.AddValue ("y-size", "Number of rows in a grid. [6]", m_ySize);
+  cmd.AddValue ("step",   "Size of edge in our grid, meters. [100 m]", m_step);
+  /*
+   * As soon as starting node means that it sends a beacon,
+   * simultaneous start is not good.
+   */
+  cmd.AddValue ("speed", "Speed of each node", m_speed);
+  cmd.AddValue ("pause", "Time for each node to pause", m_pause);
+  cmd.AddValue ("start",  "Maximum random start delay, seconds. [0.1 s]", m_randomStart);
+  cmd.AddValue ("time",  "Simulation time, seconds [100 s]", m_totalTime);
+  cmd.AddValue ("packet-interval",  "Interval between packets in UDP ping, seconds [0.001 s]", m_packetInterval);
+  cmd.AddValue ("packet-size",  "Size of packets in UDP ping", m_packetSize);
+  cmd.AddValue ("interfaces", "Number of radio interfaces used by each mesh point. [1]", m_nIfaces);
+  cmd.AddValue ("nodes", "Number of nodes in mesh", m_nNodes);
+  cmd.AddValue ("channels",   "Use different frequency channels for different interfaces. [0]", m_chan);
+  cmd.AddValue ("pcap",   "Enable PCAP traces on interfaces. [0]", m_pcap);
+  cmd.AddValue ("stack",  "Type of protocol stack. ns3::Dot11sStack by default", m_stack);
+  cmd.AddValue ("root", "Mac address of root mesh point in HWMP", m_root);
+
+  cmd.Parse (argc, argv);
+  std::cout << "Grid:" << m_xSize << "*" << m_ySize << '\n';
+  std::cout << "Simulation time: " << m_totalTime << " s\n";
+}
+void
+MeshTest::CreateNodes ()
+{
+  nodes.Create (m_nNodes);
+  // Configure YansWifiChannel
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  /*
+   * Create mesh helper and set stack installer to it
+   * Stack installer creates all needed protocols and install them to
+   * mesh point device
+   */
+  mesh = MeshHelper::Default ();
+  if (!Mac48Address (m_root.c_str ()).IsBroadcast ())
+    {
+      mesh.SetStackInstaller (m_stack, "Root", Mac48AddressValue (Mac48Address (m_root.c_str ())));
+    }
+  else
+    {
+      //If root is not set, we do not use "Root" attribute, because it
+      //is specified only for 11s
+      mesh.SetStackInstaller (m_stack);
+    }
+  if (m_chan)
+    {
+      mesh.SetSpreadInterfaceChannels (MeshHelper::SPREAD_CHANNELS);
+    }
+  else
+    {
+      mesh.SetSpreadInterfaceChannels (MeshHelper::ZERO_CHANNEL);
+    }
+  mesh.SetMacType ("RandomStart", TimeValue (Seconds (m_randomStart)));
+  // Set number of interfaces - default is single-interface mesh point
+  mesh.SetNumberOfInterfaces (m_nIfaces);
+  // Install protocols and return container if MeshPointDevices
+  meshDevices = mesh.Install (wifiPhy, nodes);
+
+  // Mobility
+  MobilityHelper mobilityAdhoc;
+  int64_t streamIndex = 0; // used to get consistent mobility across scenarios
+
+  ObjectFactory pos;
+  pos.SetTypeId ("ns3::RandomRectanglePositionAllocator");
+  std::stringstream ssX;
+  ssX << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_xSize * m_step << "]";
+  std::cout << "x here " << ssX.str();
+  pos.Set ("X", StringValue (ssX.str ()));
+
+  std::stringstream ssY;
+  ssY << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_ySize * m_step << "]";
+  std::cout << ssY.str();
+  pos.Set ("Y", StringValue (ssY.str ()));
+
+  Ptr<PositionAllocator> taPositionAlloc = pos.Create ()->GetObject<PositionAllocator> ();
+  streamIndex += taPositionAlloc->AssignStreams (streamIndex);
+
+  std::stringstream ssSpeed;
+  ssSpeed << "ns3::UniformRandomVariable[Min=0.0|Max=" << m_speed << "]";
+  std::stringstream ssPause;
+  ssPause << "ns3::ConstantRandomVariable[Constant=" << m_pause << "]";
+  mobilityAdhoc.SetMobilityModel ("ns3::RandomWaypointMobilityModel",
+                                  "Speed", StringValue (ssSpeed.str ()),
+                                  "Pause", StringValue (ssPause.str ()),
+                                  "PositionAllocator", PointerValue (taPositionAlloc));
+  mobilityAdhoc.SetPositionAllocator (taPositionAlloc);
+  mobilityAdhoc.Install (nodes);
+  streamIndex += mobilityAdhoc.AssignStreams (nodes, streamIndex);
+
+  if (m_pcap)
+    wifiPhy.EnablePcapAll (std::string ("mp-"));
+}
+void
+MeshTest::InstallInternetStack ()
+{
+  InternetStackHelper internetStack;
+  internetStack.Install (nodes);
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  interfaces = address.Assign (meshDevices);
+}
+void
+MeshTest::InstallApplication ()
+{
+  int destIdx = 0;
+
+  UdpServerHelper server (9);
+  ApplicationContainer serverApps = server.Install (nodes.Get (destIdx));
+  serverApps.Start (Seconds (0.0));
+  serverApps.Stop (Seconds (m_totalTime));
+
+  // ApplicationContainer clientApp = echoClient.Install(nodes.Get(destIdx - 1));
+  // clientApp.Start (Seconds (0.0));
+  // clientApp.Stop (Seconds (m_totalTime));
+
+  for(int i = 0; i < m_nNodes; i++) {
+    if (i != destIdx) {
+      UdpClientHelper client (interfaces.GetAddress (0), 9);
+      client.SetAttribute ("MaxPackets", UintegerValue ((uint32_t)(m_totalTime*(1/m_packetInterval))));
+      client.SetAttribute ("Interval", TimeValue (Seconds (m_packetInterval)));
+      client.SetAttribute ("PacketSize", UintegerValue (m_packetSize));
+      ApplicationContainer clientApp = client.Install(nodes.Get(i));
+      clientApp.Start (Seconds (0.0));
+      clientApp.Stop (Seconds (m_totalTime));
+    }
+  }
+}
+int
+MeshTest::Run ()
+{
+  CreateNodes ();
+  InstallInternetStack ();
+  InstallApplication ();
+  Simulator::Schedule (Seconds (m_totalTime), &MeshTest::Report, this);
+  Simulator::Stop (Seconds (m_totalTime));
+
+  PrintMap();
+
+  AnimationInterface anim ("mesh-animation.xml");
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
+}
+void
+MeshTest::Report ()
+{
+  unsigned n (0);
+  for (NetDeviceContainer::Iterator i = meshDevices.Begin (); i != meshDevices.End (); ++i, ++n)
+    {
+      std::ostringstream os;
+      os << "mp-report-" << n << ".xml";
+      std::cerr << "Printing mesh point device #" << n << " diagnostics to " << os.str () << "\n";
+      std::ofstream of;
+      of.open (os.str ().c_str ());
+      if (!of.is_open ())
+        {
+          std::cerr << "Error: Can't open file " << os.str () << "\n";
+          return;
+        }
+      mesh.Report (*i, of);
+      of.close ();
+      break;
+    }
+}
+
+void
+MeshTest::PrintMap()
+{
+  //AnimationInterface anim ("mesh-animation.xml");
+  //anim.SetConstantPosition (nodes.Get(1), 100, 200);
+    // Creates SIGSEGV error at runtime
+}
+
+int
+main (int argc, char *argv[])
+{
+  LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
+  LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
+  MeshTest t; 
+  t.Configure (argc, argv);
+  return t.Run ();
+}
